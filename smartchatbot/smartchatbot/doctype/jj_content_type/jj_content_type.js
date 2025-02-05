e0bcf98869de1f19be4c49b97112c8020ed29d60
tonjingjai <nattapong@jingjaisoft.com>
2025-02-05 15:50:47 +0700

    init project

---
+++ smartchatbot/smartchatbot/doctype/jj_content_type/jj_content_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, jingjai soft and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("JJ Content Type", {
+// 	refresh(frm) {
+
+// 	},
+// });
